89c886b581ad68c0c2ac046f23cc4d05f7ce83d1
Amr El Mawardy <amr.elmawardy94@yahoo.com>
2018-03-29 16:18:22 +0200

    workbox routing الحمد لله :)

--- custom-sw.js
+++ custom-sw.js
@@ -1,7 +1,15 @@
 importScripts('/node_modules/workbox-sw/build/workbox-sw.js')
+// Force production builds
+// workbox.setConfig({ debug: false });
+
+
+workbox.core.setCacheNameDetails({
+  prefix: 'Weather',
+  suffix: 'v1'
+});
+
 
 workbox.precaching.precacheAndRoute([])
-
 
 workbox.routing.registerRoute(
     new RegExp('.*\.svg'),
@@ -14,5 +22,48 @@ workbox.routing.registerRoute(
       if (url.href.startsWith("https://publicdata-weather.firebaseio.com/")) return true;
       return false;
     },
-    workbox.strategies.networkFirst()
+    workbox.strategies.networkFirst({
+          // Use a custom cache for this route
+          cacheName: 'json-response',
+          // Add an array of custom plugins (like workbox.expiration.Plugin)
+          plugins: []
+    })
   );
+
+
+  // custom response
+  workbox.routing.registerRoute(
+    new RegExp('.*\.png'),
+    ({url, event}) => {
+      return fetch('./images/clear.png')
+    }
+  )
+
+
+// // One thing to note is that if you return a value in the match callback, it’ll be passed into the handler callback as a params argument.
+  // workbox.routing.registerRoute(
+  //     ({url, event}) => {
+  //       return {
+  //         name: 'Workbox',
+  //         type: 'guide',
+  //       };
+  //     },
+  //     ({url, event, params}) => {
+  //       // Response will be “A guide on Workbox”
+  //     return new Response(
+  //       `A ${params.type} on ${params.name}`
+  //     );
+  //   }
+  // );
+
+
+
+// //You can customize the entire cache name by parsing in a precache and / or runtime parameter.
+
+// workbox.core.setCacheNameDetails({
+//   prefix: 'my-app',
+//   suffix: 'v1',
+//   precache: 'custom-precache-name',
+//   runtime: 'custom-runtime-name'
+// });
+
--- workbox-sw.js
+++ workbox-sw.js
@@ -1,4 +1,13 @@
 importScripts('/node_modules/workbox-sw/build/workbox-sw.js')
+// Force production builds
+workbox.setConfig({ debug: false });
+
+
+workbox.core.setCacheNameDetails({
+  prefix: 'Weather',
+  suffix: 'v1'
+});
+
 
 workbox.precaching.precacheAndRoute([
   {
@@ -15,10 +24,9 @@ workbox.precaching.precacheAndRoute([
   },
   {
     "url": "scripts/dexie.min.js",
-    "revision": "58117347fa3eaec207482f5d117b966c"
+    "revision": "06737296393e812c1406d355510bba47"
   }
 ])
-
 
 workbox.routing.registerRoute(
     new RegExp('.*\.svg'),
@@ -31,5 +39,48 @@ workbox.routing.registerRoute(
       if (url.href.startsWith("https://publicdata-weather.firebaseio.com/")) return true;
       return false;
     },
-    workbox.strategies.networkFirst()
+    workbox.strategies.networkFirst({
+          // Use a custom cache for this route
+          cacheName: 'json-response',
+          // Add an array of custom plugins (like workbox.expiration.Plugin)
+          plugins: []
+    })
   );
+
+
+  // custom response
+  workbox.routing.registerRoute(
+    new RegExp('.*\.png'),
+    ({url, event}) => {
+      return fetch('./images/clear.png')
+    }
+  )
+
+
+// // One thing to note is that if you return a value in the match callback, it’ll be passed into the handler callback as a params argument.
+  // workbox.routing.registerRoute(
+  //     ({url, event}) => {
+  //       return {
+  //         name: 'Workbox',
+  //         type: 'guide',
+  //       };
+  //     },
+  //     ({url, event, params}) => {
+  //       // Response will be “A guide on Workbox”
+  //     return new Response(
+  //       `A ${params.type} on ${params.name}`
+  //     );
+  //   }
+  // );
+
+
+
+// //You can customize the entire cache name by parsing in a precache and / or runtime parameter.
+
+// workbox.core.setCacheNameDetails({
+//   prefix: 'my-app',
+//   suffix: 'v1',
+//   precache: 'custom-precache-name',
+//   runtime: 'custom-runtime-name'
+// });
+
